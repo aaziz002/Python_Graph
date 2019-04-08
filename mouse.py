d5584bad23d13af33f62e1ee0d6fdbf7de247941
Shadowb170 <Shadowb170@gmail.com>
2019-04-08 09:57:33 -0700

    dummy file

--- mouse.py
+++ mouse.py
@@ -1,3 +1,5 @@
+
+#DUMMY FILE FOR TESTING METHODS
 import pygame, sys
 from pygame import *
 
@@ -7,21 +9,33 @@ DISPLAYSURF = pygame.display.set_mode((800, 600))
 while True: # main game loop
 	for event in pygame.event.get(): # event handling loop
 		if event.type == QUIT:
-			sys.terminate()
+			pygame.quit()
+			sys.exit()
 		elif event.type == MOUSEBUTTONDOWN:
 			print(pygame.mouse.get_pressed())
 		elif event.type == KEYDOWN:
-			if (event.key == K_LEFT or event.key == K_a) and direction != RIGHT:
-				direction = LEFT
-			elif (event.key == K_RIGHT or event.key == K_d) and direction != LEFT:
-				direction = RIGHT
-			elif (event.key == K_UP or event.key == K_w) and direction != DOWN:
-				direction = UP
-			elif (event.key == K_DOWN or event.key == K_s) and direction != UP:
-				direction = DOWN
+			if (event.key == K_LEFT or event.key == K_a)
+				print(event.key)
+			elif (event.key == K_RIGHT or event.key == K_d)
+				print(event.key)
+			elif (event.key == K_UP or event.key == K_w)
+				print(event.key)
+			elif (event.key == K_DOWN or event.key == K_s)
+				print(event.key)
 			elif (event.key == K_n):
 				all_keys = pygame.key.get_pressed()
 				if all_keys[pygame.K_d] and all_keys[pygame.K_a]:
-					print(0)
+					print(all_keys)
 			elif event.key == K_ESCAPE:
-				sys.terminate()
+				pygame.quit()
+				sys.exit()
+'''
+pygame.mouse.get_pressed	—	get the state of the mouse buttons
+pygame.mouse.get_pos	—	get the mouse cursor position
+pygame.mouse.get_rel	—	get the amount of mouse movement
+pygame.mouse.set_pos	—	set the mouse cursor position
+pygame.mouse.set_visible	—	hide or show the mouse cursor
+pygame.mouse.get_focused	—	check if the display is receiving mouse input
+pygame.mouse.set_cursor	—	set the image for the system mouse cursor
+pygame.mouse.get_cursor	—	get the image for the system mouse cursor
+'''
