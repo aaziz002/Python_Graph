6ee1a06a61a2a3ff32f38d301d110712a804d99f
Shadowb170 <Shadowb170@gmail.com>
2019-04-12 10:51:08 -0700

    push

--- crosswordpuzzle.py
+++ crosswordpuzzle.py
@@ -1,9 +1,10 @@
 import pygame, sys
+#import WordCrapes
 from pygame.locals import *
 
 # Dummy function
-def hello():
-    print("hello")
+def doNothing():
+    x = None
 
 def main():
     pygame.init()
@@ -14,26 +15,93 @@ def main():
             if event.type == QUIT:
                 terminate()
             elif event.type == MOUSEBUTTONDOWN:
-                print(pygame.mouse.get_pressed())
-            elif event.type == MOUSEMOTION:
+                #print(pygame.mouse.get_pressed())
+                button(pygame.mouse.get_pressed())
+            elif event.type == MOUSEMOTION: #tracks the  mouse position
                 mouse_x, mouse_y = pygame.mouse.get_pos()
                 position = (mouse_x, mouse_y)
-                print(position)
             elif event.type == KEYDOWN:
-                if (event.key == K_LEFT or event.key == K_a):
-                    hello()
-                elif (event.key == K_RIGHT or event.key == K_d):
-                    hello()
-                elif (event.key == K_UP or event.key == K_w):
-                    hello()
-                elif (event.key == K_DOWN or event.key == K_s):
-                    hello()
-                elif (event.key == K_n):
-                    all_keys = pygame.key.get_pressed()
-                    if all_keys[pygame.K_d] and all_keys[pygame.K_a]:
-                        hello()
-                elif event.key == K_ESCAPE:
-                    terminate()
+                button(event.key)
+
+#This function can be keyed to anything we desire.
+def button(key):
+    #keyboard section
+    if (key == K_LEFT or key == K_a):
+        doNothing()
+    elif (key == K_RIGHT or key == K_d):
+        doNothing()
+    elif (key == K_UP or key == K_w):
+        doNothing()
+    elif (key == K_DOWN or key == K_s):
+        doNothing()
+    elif (key == K_LALT or K_RALT): # section for key combinations
+        all_keys = pygame.key.get_pressed()
+        if all_keys[pygame.K_RETURN]:
+            print("screen change")
+    elif (key == K_q):
+        doNothing()
+    elif (key == K_e):
+        doNothing()
+    elif (key == K_r):
+        doNothing()
+    elif (key == K_t):
+        doNothing()
+    elif (key == K_y):
+        doNothing()
+    elif (key == K_u):
+        doNothing()
+    elif (key == K_i):
+        doNothing()
+    elif (key == K_o):
+        doNothing()
+    elif (key == K_p):
+        doNothing()
+    elif (key == K_f):
+        doNothing()
+    elif (key == K_g):
+        doNothing()
+    elif (key == K_h):
+        doNothing()
+    elif (key == K_j):
+        doNothing()
+    elif (key == K_k):
+        doNothing()
+    elif (key == K_l):
+        doNothing()
+    elif (key == K_z):
+        doNothing()
+    elif (key == K_x):
+        doNothing()
+    elif (key == K_c):
+        doNothing()
+    elif (key == K_v):
+        doNothing()
+    elif (key == K_b):
+        doNothing()
+    elif (key == K_n):
+        doNothing()
+    elif (key == K_m):
+        doNothing()
+    elif (key == K_q):
+        doNothing()
+    elif (key == K_1 or key == K_2 or key == K_3 or
+          key == K_4 or key == K_5 or key == K_6 or
+          key == K_7 or key == K_8 or key == K_9 or
+          key == K_0 or key == K_INSERT or key == K_HOME or
+          key == K_PAGEUP or key == K_PAGEDOWN or key == K_DELETE or
+          key == K_END or key == K_NUMLOCK or key == K_TAB
+          ):
+        doNothing()  #invalid key section until we need them for something
+    elif key == K_ESCAPE:
+        terminate()
+    elif (key == (1,0,0)): #Mouse section
+        doNothing() #left click
+    elif (key == (0,0,1)):
+        doNothing() #right click
+    elif (key == (0,1,0)):
+        doNothing() #middle click
+ #   elif event.type == MOUSEBUTTONDOWN: #Mouse section
+  #        print(pygame.mouse.get_pressed())
 
 def terminate():
     pygame.quit()
